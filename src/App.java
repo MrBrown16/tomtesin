29bf2b2d7bd2397e1bddf95f63c4fb2b0080e0dd
Szabó Szebasztián <szaboszebasztianlevente@ktch.hu>
2023-04-21 12:56:54 +0200

    app

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
-public class App {
+import control.MainControl;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
     public static void main(String[] args) throws Exception {
-        System.out.println("BU!");
+        
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        MainControl mainControl = new MainControl();
+        Scene scene = new Scene(mainControl.getMainView(), 300, 400);
+        
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
